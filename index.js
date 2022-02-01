f4b911a0cb7a4a9993fd8ace375edc276cc1c9a3
Bevin Duncan <bevin.duncan@gmail.com>
2022-01-31 22:38:43 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+}
+function mondayWork(workActivity = 'go to the office'){
+    return (`This Monday, I will ${workActivity}.`)
+}
+function wrapAdjective(flair = '*'){
+    function innerFunction(p1 = 'special'){
+        return `You are ${flair}${p1}${flair}!`
+    }
+        return innerFunction
+}
+wrapAdjective("%")("a dedicated programmer");
